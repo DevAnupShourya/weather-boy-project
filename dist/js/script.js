a163948070ea9cd429ba102c15bb8094a1a683da
Anup Shourya <contact.yourwebdev@gmail.com>
2023-02-02 13:54:06 +0530

    front-end complete

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Hello World');
+// ! for frone-end only
+const menu_open_btn = document.getElementById('menu_open_btn');
+const menu_close_btn = document.getElementById('menu_close_btn');
+const navbar = document.getElementById('navbar');
+menu_open_btn.addEventListener('click', () => {
+    navbar.style.display = 'flex';
+});
+menu_close_btn.addEventListener('click', () => {
+    navbar.style.display = 'none';
+});
--- src/typescript/script.ts
+++ src/typescript/script.ts
@@ -1,1 +1,11 @@
 // ! for frone-end only
+const menu_open_btn: HTMLElement = document.getElementById('menu_open_btn')!;
+const menu_close_btn: HTMLElement = document.getElementById('menu_close_btn')!;
+const navbar: HTMLElement = document.getElementById('navbar')!;
+
+menu_open_btn.addEventListener('click', () => {
+    navbar.style.display = 'flex';
+})
+menu_close_btn.addEventListener('click', () => {
+    navbar.style.display = 'none';
+})
